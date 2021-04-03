844fa72fb4f0bab4914eb3be6a23857fcdc2b56e
serhii.buhlak <serhii.buhlak@socialtechnologies.io>
2021-04-03 18:05:50 +0300

    Add fix for BadClass

--- homework_5/bad_class.py
+++ homework_5/bad_class.py
@@ -3,11 +3,11 @@ class BadClass:
     def __init__(self, val=0):
         self.val = val
 
-    def __hash__(self):
-        return hash(self.val)
-
-    def __eq__(self, other):
-        return self.val == other.val
+    # def __hash__(self):
+    #     return hash(self.val)
+    #
+    # def __eq__(self, other):
+    #     return self.val == other.val
 
 
 bad = BadClass()
