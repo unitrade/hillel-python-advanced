47f58dee75992a75a7fe0519f6261d1dd040ae77
serhii.buhlak <serhii.buhlak@socialtechnologies.io>
2021-04-11 17:40:16 +0300

    Modify copy_with method

--- homework_7/person.py
+++ homework_7/person.py
@@ -25,22 +25,34 @@ class Person:
     def get_gender(self):
         return self.__gender
 
-    def copy_with(self, name, last_name):
+    def copy_with(self, **kwargs):
         my_object = copy(self)
         my_object.__id = uuid.uuid1()
-        my_object.__name = name
-        my_object.__last_name = last_name
+        for key, value in kwargs.items():
+            try:
+                if key == 'id':
+                    print('ID is autogenerate field')
+                elif key == 'name':
+                    my_object.__name = value
+                elif key == 'last_name':
+                    my_object.__last_name = value
+                elif key == 'gender':
+                    my_object.__gender = value
+                else:
+                    raise NameError("name '{}' is not defined".format(key))
+            except NameError as err:
+                print(err)
         return my_object
 
     def get_full_info(self):
-        return "UUID: %s\nName: %s\nSurname: %s\nGender: %s " \
+        return "+++++++++++++++++++++\nUUID: %s\nName: %s\nSurname: %s\nGender: %s " \
                % (self.__id, self.__name, self.__last_name, self.__gender)
 
     def __hash__(self):
         return hash(self.__id)
 
     def __eq__(self, other):
-        return self.__last_name == other.__last_name and self.__name == other.__name and self.__gender == other.__gender
+        return self.__id == other.__id
 
 
 if __name__ == '__main__':
@@ -62,26 +74,31 @@ if __name__ == '__main__':
         'male'
     )
 
-    assert person1 == person3, 'Object not equal'
-
     person1.__setattr__("name", "Alex")
     assert person1.get_name() == 'Silvester', 'Object has been changed'
 
     person4 = person1.copy_with(
-        "Brad",
-        "Pitt"
+        id="123",
+        city="New York",
+        name="Rihanna",
+        last_name="Fenty",
+        gender='female',
+        birthday="1988"
     )
 
     print(person1.get_full_info())
+    print(person4.get_full_info())
 
-    # UUID: 343b4f7a-9936-11eb-a497-acde48001122
+    # ID is autogenerate field
+    # name 'city' is not defined
+    # name 'birthday' is not defined
+    # +++++++++++++++++++++
+    # UUID: 809ca276-9ad3-11eb-b421-acde48001122
     # Name: Silvester
     # Surname: Stallone
     # Gender: male
-
-    print(person4.get_full_info())
-
-    # UUID: 343b520e-9936-11eb-a497-acde48001122
-    # Name: Brad
-    # Surname: Pitt
-    # Gender: male
+    # +++++++++++++++++++++
+    # UUID: 809ca4f6-9ad3-11eb-b421-acde48001122
+    # Name: Rihanna
+    # Surname: Fenty
+    # Gender: female
